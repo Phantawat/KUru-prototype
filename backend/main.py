dbba3eb535faa4149c70d48527ae92a73eec9428
Phantawat <phantawat.lu@gmail.com>
2026-03-23 10:43:00 +0700

    fix: update CORS settings to allow all origins

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ from recommendation_pipeline import RecommendationPipeline
 app = FastAPI()
 app.add_middleware(
 	CORSMiddleware,
-	allow_origins=["http://localhost:3000"],
+	allow_origins=["*"],
 	allow_methods=["*"],
 	allow_headers=["*"],
 )
